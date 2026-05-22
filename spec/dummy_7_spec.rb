4d46050f58fdc86fda65aaaa1adbcfcdb387c3c4
nuvanda <jax.nuvanda@gmail.com>
2026-05-22 23:34:48 +0300

    Add slow dummy tests

---
+++ spec/dummy_7_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe "Dummy 7" do
+  it "sleeps for 100 seconds" do
+    sleep(20)
+    expect(true).to be true
+  end
+
+  it "sleeps for another 100 seconds" do
+    sleep(20)
+    expect(true).to be true
+  end
+end
